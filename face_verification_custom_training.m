b80a2c17eff97b2847aae87c618702934687bccc
Aks Yadav <123442534+akstheme@users.noreply.github.com>
2024-05-27 16:52:53 +0530

    Add files via upload

---
+++ face_verification_custom_training.m
@@ -0,0 +1,242 @@
+clc;
+close all;
+clear all;
+
+% Load pre-trained SqueezeNet
+net = squeezenet;
+
+% Define the main dataset directory
+datasetDir = 'D:\Aks\AKS_SYSTEM\Computer vision based research\My_work\Databases\Proposed_FER\IITD_FER_Flat';
+
+% Create an imageDatastore for the entire dataset
+imds = imageDatastore(datasetDir, 'IncludeSubfolders', true, 'LabelSource', 'foldernames');
+
+% Split the dataset into training and validation sets (80% training, 20% validation)
+[imdsTrain, imdsVal] = splitEachLabel(imds, 0.8, 'randomized');
+
+% Modify the network for transfer learning
+numClasses = numel(categories(imdsTrain.Labels));
+%% Network design
+% Create layer graph
+lgraph = layerGraph();
+%-----------------------------------------------------------------
+% add layers
+tempLayers = [
+    imageInputLayer([227 227 3],"Name","data")
+    convolution2dLayer([3 3],64,"Name","conv1","Stride",[2 2])
+    reluLayer("Name","relu_conv1")
+    maxPooling2dLayer([3 3],"Name","pool1","Stride",[2 2])
+    convolution2dLayer([1 1],16,"Name","fire2-squeeze1x1")
+    reluLayer("Name","fire2-relu_squeeze1x1")];
+lgraph = addLayers(lgraph,tempLayers);
+
+tempLayers = [
+    convolution2dLayer([1 1],64,"Name","fire2-expand1x1")
+    reluLayer("Name","fire2-relu_expand1x1")];
+lgraph = addLayers(lgraph,tempLayers);
+
+tempLayers = [
+    convolution2dLayer([3 3],64,"Name","fire2-expand3x3","Padding",[1 1 1 1])
+    reluLayer("Name","fire2-relu_expand3x3")];
+lgraph = addLayers(lgraph,tempLayers);
+
+tempLayers = [
+    depthConcatenationLayer(2,"Name","fire2-concat")
+    convolution2dLayer([1 1],16,"Name","fire3-squeeze1x1")
+    reluLayer("Name","fire3-relu_squeeze1x1")];
+lgraph = addLayers(lgraph,tempLayers);
+
+tempLayers = [
+    convolution2dLayer([1 1],64,"Name","fire3-expand1x1")
+    reluLayer("Name","fire3-relu_expand1x1")];
+lgraph = addLayers(lgraph,tempLayers);
+
+tempLayers = [
+    convolution2dLayer([3 3],64,"Name","fire3-expand3x3","Padding",[1 1 1 1])
+    reluLayer("Name","fire3-relu_expand3x3")];
+lgraph = addLayers(lgraph,tempLayers);
+
+tempLayers = [
+    depthConcatenationLayer(2,"Name","fire3-concat")
+    maxPooling2dLayer([3 3],"Name","pool3","Padding",[0 1 0 1],"Stride",[2 2])
+    convolution2dLayer([1 1],32,"Name","fire4-squeeze1x1")
+    reluLayer("Name","fire4-relu_squeeze1x1")];
+lgraph = addLayers(lgraph,tempLayers);
+
+tempLayers = [
+    convolution2dLayer([1 1],128,"Name","fire4-expand1x1")
+    reluLayer("Name","fire4-relu_expand1x1")];
+lgraph = addLayers(lgraph,tempLayers);
+
+tempLayers = [
+    convolution2dLayer([3 3],128,"Name","fire4-expand3x3","Padding",[1 1 1 1])
+    reluLayer("Name","fire4-relu_expand3x3")];
+lgraph = addLayers(lgraph,tempLayers);
+
+tempLayers = [
+    depthConcatenationLayer(2,"Name","fire4-concat")
+    convolution2dLayer([1 1],32,"Name","fire5-squeeze1x1")
+    reluLayer("Name","fire5-relu_squeeze1x1")];
+lgraph = addLayers(lgraph,tempLayers);
+
+tempLayers = [
+    convolution2dLayer([1 1],128,"Name","fire5-expand1x1")
+    reluLayer("Name","fire5-relu_expand1x1")];
+lgraph = addLayers(lgraph,tempLayers);
+
+tempLayers = [
+    convolution2dLayer([3 3],128,"Name","fire5-expand3x3","Padding",[1 1 1 1])
+    reluLayer("Name","fire5-relu_expand3x3")];
+lgraph = addLayers(lgraph,tempLayers);
+
+tempLayers = [
+    depthConcatenationLayer(2,"Name","fire5-concat")
+    maxPooling2dLayer([3 3],"Name","pool5","Padding",[0 1 0 1],"Stride",[2 2])
+    convolution2dLayer([1 1],48,"Name","fire6-squeeze1x1")
+    reluLayer("Name","fire6-relu_squeeze1x1")];
+lgraph = addLayers(lgraph,tempLayers);
+
+tempLayers = [
+    convolution2dLayer([1 1],192,"Name","fire6-expand1x1")
+    reluLayer("Name","fire6-relu_expand1x1")];
+lgraph = addLayers(lgraph,tempLayers);
+
+tempLayers = [
+    convolution2dLayer([3 3],192,"Name","fire6-expand3x3","Padding",[1 1 1 1])
+    reluLayer("Name","fire6-relu_expand3x3")];
+lgraph = addLayers(lgraph,tempLayers);
+
+tempLayers = [
+    depthConcatenationLayer(2,"Name","fire6-concat")
+    convolution2dLayer([1 1],48,"Name","fire7-squeeze1x1")
+    reluLayer("Name","fire7-relu_squeeze1x1")];
+lgraph = addLayers(lgraph,tempLayers);
+
+tempLayers = [
+    convolution2dLayer([1 1],192,"Name","fire7-expand1x1")
+    reluLayer("Name","fire7-relu_expand1x1")];
+lgraph = addLayers(lgraph,tempLayers);
+
+tempLayers = [
+    convolution2dLayer([3 3],192,"Name","fire7-expand3x3","Padding",[1 1 1 1])
+    reluLayer("Name","fire7-relu_expand3x3")];
+lgraph = addLayers(lgraph,tempLayers);
+
+tempLayers = [
+    depthConcatenationLayer(2,"Name","fire7-concat")
+    convolution2dLayer([1 1],64,"Name","fire8-squeeze1x1")
+    reluLayer("Name","fire8-relu_squeeze1x1")];
+lgraph = addLayers(lgraph,tempLayers);
+
+tempLayers = [
+    convolution2dLayer([1 1],256,"Name","fire8-expand1x1")
+    reluLayer("Name","fire8-relu_expand1x1")];
+lgraph = addLayers(lgraph,tempLayers);
+
+tempLayers = [
+    convolution2dLayer([3 3],256,"Name","fire8-expand3x3","Padding",[1 1 1 1])
+    reluLayer("Name","fire8-relu_expand3x3")];
+lgraph = addLayers(lgraph,tempLayers);
+
+tempLayers = [
+    depthConcatenationLayer(2,"Name","fire8-concat")
+    convolution2dLayer([1 1],64,"Name","fire9-squeeze1x1")
+    reluLayer("Name","fire9-relu_squeeze1x1")];
+lgraph = addLayers(lgraph,tempLayers);
+
+tempLayers = [
+    convolution2dLayer([1 1],256,"Name","fire9-expand1x1")
+    reluLayer("Name","fire9-relu_expand1x1")];
+lgraph = addLayers(lgraph,tempLayers);
+
+tempLayers = [
+    convolution2dLayer([3 3],256,"Name","fire9-expand3x3","Padding",[1 1 1 1])
+    reluLayer("Name","fire9-relu_expand3x3")];
+lgraph = addLayers(lgraph,tempLayers);
+
+tempLayers = [
+    depthConcatenationLayer(2,"Name","fire9-concat")
+    dropoutLayer(0.5,"Name","drop9")
+    convolution2dLayer([1 1],18,"Name","conv")
+    reluLayer("Name","relu_conv10")
+    globalAveragePooling2dLayer("Name","pool10")
+    softmaxLayer("Name","prob")
+    classificationLayer("Name","classoutput")];
+lgraph = addLayers(lgraph,tempLayers);
+
+% clean up helper variable
+clear tempLayers;
+% -------------------------------------------------------------
+% connect layer branches
+lgraph = connectLayers(lgraph,"fire2-relu_squeeze1x1","fire2-expand1x1");
+lgraph = connectLayers(lgraph,"fire2-relu_squeeze1x1","fire2-expand3x3");
+lgraph = connectLayers(lgraph,"fire2-relu_expand1x1","fire2-concat/in1");
+lgraph = connectLayers(lgraph,"fire2-relu_expand3x3","fire2-concat/in2");
+lgraph = connectLayers(lgraph,"fire3-relu_squeeze1x1","fire3-expand1x1");
+lgraph = connectLayers(lgraph,"fire3-relu_squeeze1x1","fire3-expand3x3");
+lgraph = connectLayers(lgraph,"fire3-relu_expand1x1","fire3-concat/in1");
+lgraph = connectLayers(lgraph,"fire3-relu_expand3x3","fire3-concat/in2");
+lgraph = connectLayers(lgraph,"fire4-relu_squeeze1x1","fire4-expand1x1");
+lgraph = connectLayers(lgraph,"fire4-relu_squeeze1x1","fire4-expand3x3");
+lgraph = connectLayers(lgraph,"fire4-relu_expand1x1","fire4-concat/in1");
+lgraph = connectLayers(lgraph,"fire4-relu_expand3x3","fire4-concat/in2");
+lgraph = connectLayers(lgraph,"fire5-relu_squeeze1x1","fire5-expand1x1");
+lgraph = connectLayers(lgraph,"fire5-relu_squeeze1x1","fire5-expand3x3");
+lgraph = connectLayers(lgraph,"fire5-relu_expand1x1","fire5-concat/in1");
+lgraph = connectLayers(lgraph,"fire5-relu_expand3x3","fire5-concat/in2");
+lgraph = connectLayers(lgraph,"fire6-relu_squeeze1x1","fire6-expand1x1");
+lgraph = connectLayers(lgraph,"fire6-relu_squeeze1x1","fire6-expand3x3");
+lgraph = connectLayers(lgraph,"fire6-relu_expand1x1","fire6-concat/in1");
+lgraph = connectLayers(lgraph,"fire6-relu_expand3x3","fire6-concat/in2");
+lgraph = connectLayers(lgraph,"fire7-relu_squeeze1x1","fire7-expand1x1");
+lgraph = connectLayers(lgraph,"fire7-relu_squeeze1x1","fire7-expand3x3");
+lgraph = connectLayers(lgraph,"fire7-relu_expand1x1","fire7-concat/in1");
+lgraph = connectLayers(lgraph,"fire7-relu_expand3x3","fire7-concat/in2");
+lgraph = connectLayers(lgraph,"fire8-relu_squeeze1x1","fire8-expand1x1");
+lgraph = connectLayers(lgraph,"fire8-relu_squeeze1x1","fire8-expand3x3");
+lgraph = connectLayers(lgraph,"fire8-relu_expand1x1","fire8-concat/in1");
+lgraph = connectLayers(lgraph,"fire8-relu_expand3x3","fire8-concat/in2");
+lgraph = connectLayers(lgraph,"fire9-relu_squeeze1x1","fire9-expand1x1");
+lgraph = connectLayers(lgraph,"fire9-relu_squeeze1x1","fire9-expand3x3");
+lgraph = connectLayers(lgraph,"fire9-relu_expand1x1","fire9-concat/in1");
+lgraph = connectLayers(lgraph,"fire9-relu_expand3x3","fire9-concat/in2");
+%-----------------------------------------------------------------------
+plot(lgraph);
+
+% Set training options
+options = trainingOptions('adam', ...
+    'MiniBatchSize', 128, ...
+    'MaxEpochs', 10, ...
+    'InitialLearnRate', 1e-3, ...
+    'Shuffle', 'every-epoch', ...
+    'ValidationData', imdsVal, ...
+    'ValidationFrequency', 10, ...
+    'Verbose', true, ...
+    'Plots', 'training-progress');
+
+% Train the network
+netTransfer = trainNetwork(imdsTrain, lgraph, options);
+
+
+
+
+% Allow the user to select test images
+[testFileName, testFilePath] = uigetfile({'*.jpg;*.png;*.bmp;*.tif', 'Image Files (*.jpg, *.png, *.bmp, *.tif)'; '*.*', 'All Files (*.*)'}, 'Select Test Images', 'MultiSelect', 'on');
+if isequal(testFileName, 0)
+    error('No files selected.');
+end
+testImages = fullfile(testFilePath, testFileName);
+
+% Create an imageDatastore for the selected test images
+imdsTest = imageDatastore(testImages);
+
+% Predict on the test set
+YPred = classify(netTransfer, imdsTest);
+
+% Display predictions
+figure;
+for i = 1:min(5, numel(YPred))
+    subplot(1, 5, i);
+    imshow(imread(testImages{i}));
+    title(YPred(i));
+end
